e1ab4172a1a1cee1b9fb4ce0cb13d4a5f804c110
Federico Farías Sánchez <cobrakik01@gmail.com>
2013-06-09 15:54:11 -0500

    Se agregaron dos clases Admin y Client que gestionaran los controladores de dichos modulos

---
+++ application/libraries/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Admin {
+
+    public static function controllers() {
+        
+    }
+
+}
+
+?>
